1d7418a33010f000c1cd0fd0e19f504b292788ca
Ryan <ryan@tesorio.com>
2023-02-28 12:13:45 -0800

    updating code for homework-2

--- homework-2/classifier.py
+++ homework-2/classifier.py
@@ -1,6 +1,6 @@
 # first party
-import glob
 import os
+import sys
 
 # third party
 import numpy as np
@@ -20,6 +20,7 @@ from nltk.util import bigrams, trigrams
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
+# Set up filepath
 fpath = "ngram_authorship_train"
 
 
@@ -67,19 +68,43 @@ class WittenBellInterpolated(InterpolatedLanguageModel):
         super().__init__(WittenBell, order, **kwargs)
 
 
-def load_data(split="sentence"):
+def prepare_data(filename, author_name, split):
+    """
+    Function that prepares the data.
+    """
+    # Read in the data
+    data = open(filename).read().splitlines()
+
+    # Combine the paragrahs
+    # Note: After some thought I don't think this is the right interpretation
+    # afterall. However, I will keep the code for now
+    if split == "paragraph":
+        data = [line if line != "" else "\n" for line in data]
+        data = " ".join(data).split("\n")
+
+    elif split == "sentence":
+        data = [line for line in data if line != ""]
+        data = " ".join(data)
+        data = sent_tokenize(data)
+
+    data = [(line.lower().strip(), author_name) for line in data]
+    data = [(line, author) for line, author in data if line != ""]
+
+    return data
+
+
+def load_data(filenames, test_filenames=None, split="sentence"):
     """
     Function to load the data from the author text files. The way
     we will load in the data is to treat each paragraph as a full
     text
     """
-    # Get the different file names
-    data_dir = "./data"
-    path = os.path.join(data_dir, fpath, "*")
+    # Logging
+    print("Setting up the data")
 
     data_list = []
     authorlist = []
-    for filename in glob.glob(path):
+    for filename in filenames:
         # For now we will use the utf-8 version
         if "utf8" in filename:
             # Get the author name
@@ -87,36 +112,45 @@ def load_data(split="sentence"):
             authorlist.append(author_name)
 
             # Read in the data
-            data = open(filename).read().splitlines()
-
-            # Combine the paragrahs
-            # Note: After some thought I don't think this is the right interpretation
-            # afterall. However, I will keep the code for now
-            if split == "paragraph":
-                data = [line if line != "" else "\n" for line in data]
-                data = " ".join(data).split("\n")
-
-            elif split == "sentence":
-                data = [line for line in data if line != ""]
-                data = " ".join(data)
-                data = sent_tokenize(data)
-
-            data = [(line.lower().strip(), author_name) for line in data]
-            data = [(line, author) for line, author in data if line != ""]
+            data = prepare_data(filename, author_name=author_name, split=split)
             data_list.append(data)
 
     # Combine all of the data
     data_list = [item for sublist in data_list for item in sublist]
 
-    # Split the data into training and validation sets
-    train_data, test_data = train_test_split(
-        data_list, test_size=0.10, random_state=2023
-    )
-
-    # Put the data in a data frame
+    # Split the data into training and validation sets if there is not a test
+    # set present
     columns = ["text", "author"]
-    train_data = pd.DataFrame(train_data, columns=columns)
-    test_data = pd.DataFrame(test_data, columns=columns)
+    if test_filenames is None:
+        print("Splitting into training and development data sets ...")
+        train_data, test_data = train_test_split(
+            data_list, test_size=0.10, random_state=2023
+        )
+
+        # Put the data in a data frame
+        train_data = pd.DataFrame(train_data, columns=columns)
+        test_data = pd.DataFrame(test_data, columns=columns)
+
+    else:
+        print("Training on full data and using outside testing data ...")
+        train_data = pd.DataFrame(data_list, columns=columns)
+
+        # If there is a test file then we need to prepare this data as well
+        print("Preparing the testing data ...")
+        test_data_list = []
+        for test_filename in test_filenames:
+            # Get the author name
+            author_name = test_filename.split("/")[-1].split("_")[0]
+
+            # Prepare the data
+            test_data = prepare_data(
+                test_filename, author_name=author_name, split=split
+            )
+            test_data_list.append(test_data)
+
+        # Flatten data
+        test_data_list = [item for sublist in test_data_list for item in sublist]
+        test_data = pd.DataFrame(test_data_list, columns=columns)
 
     return train_data, test_data, authorlist
 
@@ -134,7 +168,9 @@ def train(train_data, authorlist, model_type="MLE", n=3, **kwargs):
     models = {}
 
     # Extract the data and build the model
+    print("training LMs ...")
     for author_name in authorlist:
+        print(f"training LM for {author_name}")
         # Initalize model - needs to be reinitialized for every author
         # or else we have the same model
         if model_type == "MLE":
@@ -194,6 +230,7 @@ def predict(test_data, authorlist, models):
     models = {k: v for k, v in models.items() if k != "attributes"}
 
     # Create the text & labels
+    print("Extracting dev set data ...")
     test_text = test_data["text"].tolist()
     test_text = [tokenizer.tokenize(line) for line in test_text]
 
@@ -204,6 +241,7 @@ def predict(test_data, authorlist, models):
 
     # Iterate over the test data and the models
     predictions = []
+    print("Predicting ...")
     for text in test_text:
         # Set up the bigrams / trigrams. Note that this n in
         # pad_both_ends is not the same n used to define the
@@ -234,6 +272,7 @@ def predict(test_data, authorlist, models):
 
     # Calculate the overall accuracy and the author
     # specific accuracy
+    print("Results on dev / test set")
     for index, author_name in enumerate(authorlist):
         # Get the author specific labels
         mask = labels == index
@@ -247,14 +286,45 @@ def predict(test_data, authorlist, models):
         den = len(author_labels)
         score = num / den
 
-        print(f"Accuracy for {author_name} = {score}")
+        print(f"Accuracy for {author_name} = {score * 100}%")
 
     # Get overall accuracy
     overall_score = accuracy_score(labels, predictions)
-    print(f"Overall accuracy = {overall_score}")
+    print(f"Overall accuracy = {overall_score * 100}%")
 
     return predictions, labels
 
 
+def main_task(filenames, test_filenames=None):
+    """
+    Task that performs the classification and text generation
+    """
+    MODEL_TYPE = "WittenBellInterpolated"
+    n = 3
+
+    train_data, test_data, authorlist = load_data(
+        filenames=filenames, test_filenames=test_filenames
+    )
+
+    # Gathering the models
+    models = train(train_data, authorlist, model_type=MODEL_TYPE, n=n)
+
+    # Get the predictions on the dev set
+    predictions, labels = predict(test_data, authorlist, models)
+
+
 if __name__ == "__main__":
-    pass
+    authorlist_filename_index = sys.argv.index("authorlist.txt")
+    authorlist_filename = sys.argv[authorlist_filename_index]
+    filenames = open(authorlist_filename).read().splitlines()
+
+    if "-test" not in sys.argv:
+        test_filenames = None
+
+    else:
+        test_filename_index = sys.argv.index("testfile.txt")
+        test_filename = sys.argv[test_filename_index]
+        test_filenames = open(test_filename).read().splitlines()
+
+    # Start main task
+    main_task(filenames=filenames, test_filenames=test_filenames)
