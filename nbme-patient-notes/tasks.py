346c4a8ba5be45af828f0d21824fcedf20aa7b34
Ryan Devera <dever120@umn.edu>
2023-04-21 18:46:55 -0700

    adding the pseudo-label training pipeline

--- nbme-patient-notes/tasks.py
+++ nbme-patient-notes/tasks.py
@@ -1,4 +1,8 @@
+# first party
+import ast
+
 # third party
+import pandas as pd
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -117,3 +121,83 @@ def run_model_pipeline():
         pseudo_data["pn_num"].astype(str) + "_" + pseudo_data["feature_num"].astype(str)
     )
     pseudo_data.to_csv("./nbme_data/pseudo_train.csv", index=False)
+
+
+def run_model_pipeline_with_pseudo_labels():
+    """
+    Function that runs the NBME pipeline with pseudo-labeled
+    data
+    """
+    # Load in the data
+    print("Loading configuration and data")
+    # Load in the data
+    config = Configuration()
+    data = pd.read_csv("./nbme_data/train_data_with_pseudo_labels.csv")
+    data["annotation"] = data["annotation"].apply(ast.literal_eval)
+    data["location"] = data["location"].apply(ast.literal_eval)
+
+    device = (
+        torch.device("cuda:1") if torch.cuda.is_available() else torch.device("cpu")
+    )
+
+    # Get training and validation data
+    print("Load and build data loaders for training and evaluation")
+    train_df = data.loc[data["fold_number"] != 4].reset_index(drop=True)
+    valid_df = data.loc[data["fold_number"] == 4].reset_index(drop=True)
+    valid_patient_notes_texts = valid_df["pn_history"].values
+    valid_labels = valid_df["location"].apply(create_labels_for_scoring)
+
+    # Create the datasets and data loaders
+    training_dataset = NBMEDataset(train_df, config)
+    valid_dataset = NBMEDataset(valid_df, config)
+
+    # Training loaders
+    train_loader = DataLoader(
+        training_dataset, batch_size=8, shuffle=True, pin_memory=True, drop_last=True
+    )
+    valid_loader = DataLoader(
+        valid_dataset, batch_size=8, shuffle=False, pin_memory=True, drop_last=False
+    )
+
+    # Get the loss and optimizers and model
+    model = NBMEModel(config=config)
+    model = model.to(device=device)
+
+    criterion = nn.BCEWithLogitsLoss(reduction="none")
+    optimizer = optim.AdamW(model.parameters(), lr=1e-5)
+
+    # TODO: still need to make training function and validation
+    print("Train model")
+    for epoch in range(7):
+        training_function(
+            config=config,
+            train_loader=train_loader,
+            model=model,
+            criterion=criterion,
+            optimizer=optimizer,
+            scheduler=None,
+            device=device,
+        )
+
+        # Get the probability outputs
+        predictions, labels = validation_function(config, valid_loader, model, device)
+
+        # Reshape the predictions and labels
+        samples = len(valid_df)
+        predictions = predictions.reshape((samples, config.max_length))
+        labels = labels.reshape((samples, config.max_length))
+
+        # Get character probabilities
+        character_probabilities = get_character_probabilities(
+            valid_patient_notes_texts, predictions, config
+        )
+
+        # Get results
+        results = get_thresholded_sequences(character_probabilities)
+        preds = get_predictions(results)
+        score = get_score(valid_labels, preds)
+        print(score)
+
+        # Save the model after each epoch
+        PATH = f"./models/deberta_v3_base_cpt_epoch_{epoch}_pl.pt"
+        torch.save(model.state_dict(), PATH)
